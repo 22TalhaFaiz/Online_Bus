613d2c4b74d0099b34daf670ff965a5e2f585ec5
asfahan321 <muhammadasfahan689@gmail.com>
2024-07-28 00:43:40 +0500

    signup login cmplt

--- Online_ticket-main/WebApplication7/WebApplication7/obj/Debug/net6.0/WebApplication7.AssemblyInfo.cs
+++ Online_ticket-main/WebApplication7/WebApplication7/obj/Debug/net6.0/WebApplication7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1b362a93fb01a75ef7796a194e4c30effa9060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28d85e1ba46d9c106a3deba70031d3e17fa7c87")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
