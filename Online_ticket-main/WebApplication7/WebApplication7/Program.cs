602e7f7ee59ebabdfa888ff3b0bfa15d3b8948db
Arsalan Aslam <165080231+asfahan09@users.noreply.github.com>
2024-08-02 10:52:42 +0500

    admin work

--- Online_ticket-main/WebApplication7/WebApplication7/Program.cs
+++ Online_ticket-main/WebApplication7/WebApplication7/Program.cs
@@ -29,4 +29,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Dashboard}/{action=AdminIndex}/{id?}");
+
+
 app.Run();
--- Online_ticket-main/admin33/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace admin33.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
