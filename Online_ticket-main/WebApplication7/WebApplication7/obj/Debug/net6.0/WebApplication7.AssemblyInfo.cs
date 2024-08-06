109242764f47353c4fccabcfc9e5af2893e93837
asfahan321 <muhammadasfahan689@gmail.com>
2024-08-06 15:44:59 +0500

    work

--- Online_ticket-main/WebApplication7/WebApplication7/obj/Debug/net6.0/WebApplication7.AssemblyInfo.cs
+++ Online_ticket-main/WebApplication7/WebApplication7/obj/Debug/net6.0/WebApplication7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f86eed4b03fd786fc4d518dc67f9f44a5eaf80d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6b015b63f5f7fac7b692d47badcc01b1d07c9dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
